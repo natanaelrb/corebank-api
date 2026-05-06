85d88d074d5e314f7cfab9dceea087fcf791f885
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-06 17:27:42 -0300

    chore: ajustes na inicialização da aplicação

--- src/main/java/com/natan/corebank/CorebankApplication.java
+++ src/main/java/com/natan/corebank/CorebankApplication.java
@@ -3,7 +3,6 @@ package com.natan.corebank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class CorebankApplication {
 
